122667adba36b1f4d99e681173b0fc5f1593ca2e
Joanna Yuhas <joannayuhas@gmail.com>
2019-05-02 18:39:17 -0400

    My Solution

--- github/Coding-Challenges/third_challenge.rb
+++ github/Coding-Challenges/third_challenge.rb
@@ -1,7 +1,11 @@
 def reverse(array)
-
-
-
+i = array.length
+new_array = []
+while i > 0
+  new_array << array.pop
+  i -= 1
+end
+new_array
 end
 
-array = [1,2,3,4,5,6,7,8]
+array = [1,2,3,4,5,6,7,8,9,10]
